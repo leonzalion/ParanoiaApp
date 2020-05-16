a09e6648d41c2e4c8fe550a5c4db0d594609e19c
Leon Si <leonzalion@gmail.com>
2020-05-16 19:18:50 -0400

    hi

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Paranoia</h1>
+    <calendar
+      style="height: 800px;"
+      :taskView="false"
+      view="week"
+      :scheduleView="['time']"
+      :schedules="schedules"
+      @beforeCreateSchedule="onBeforeCreateSchedule"
+      @beforeUpdateSchedule="onBeforeUpdateSchedule"
+      @beforeDeleteSchedule="onBeforeDeleteSchedule"
+    />
+    <button
+      id="lock-schedule"
+      :disabled="isScheduleLocked"
+      @click="lockSchedule"
+    >
+      Lock Schedule
+    </button>
+    <div id="screenshots">
+      <button @click="takeScreenshot">Take Screenshot(s)</button>
+      <img
+        v-for="(base64Screenshot, index) in base64Screenshots"
+        :src="base64Screenshot"
+        width="400"
+        alt="`Screenshot #${index}`"
+        :key="index"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ipcRenderer } from 'electron';
+import { Calendar } from '@toast-ui/vue-calendar';
+import Vue from 'vue';
+import cuid from 'cuid';
+import 'tui-calendar/dist/tui-calendar.css';
+
+import 'tui-date-picker/dist/tui-date-picker.css';
+import 'tui-time-picker/dist/tui-time-picker.css';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Calendar
+  },
+  data: () => ({
+    base64Screenshots: [],
+    schedules: [],
+    isScheduleLocked: false,
+  }),
+  created() {
+    ipcRenderer.on('tookScreenshot', (event, base64Screenshots) => {
+      console.log(base64Screenshots.length);
+      this.base64Screenshots = base64Screenshots;
+    });
+  },
+  methods: {
+    takeScreenshot() {
+      ipcRenderer.invoke('takeScreenshot');
+    },
+    lockSchedule() {
+      ipcRenderer.invoke('lockSchedule', this.schedules);
+    },
+    onBeforeCreateSchedule(e) {
+      this.schedules.push({
+        id: cuid(),
+        start: e.start,
+        end: e.end,
+        title: e.title,
+        category: 'time',
+      });
+    },
+    onBeforeDeleteSchedule(e) {
+      this.schedules = this.schedules.filter((schedule) => {
+        console.log(schedule.id, e.schedule.id);
+        return schedule.id !== e.schedule.id;
+      });
+    },
+    onBeforeUpdateSchedule(e) {
+      const index = this.schedules.findIndex((schedule) => schedule.id === e.schedule.id);
+      Vue.set(this.schedules, index, {...this.schedules[index], ...e.changes});
+    }
+  },
 }
 </script>
 
@@ -24,5 +95,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#screenshots {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import io from 'socket.io-client';
 
 Vue.config.productionTip = false
 
 new Vue({
+  socket: io('https://paranoia-app.herokuapp.com'),
   render: h => h(App),
 }).$mount('#app')
